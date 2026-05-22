6e99e50f29f122d3eabf1a952471291d852c82cc
ccerviedelara-lang <cc.ervie.delara@cvsu.edu.ph>
2026-05-22 08:40:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ class MobileSessionInterface(SecureCookieSessionInterface):
 
 app.session_interface = MobileSessionInterface()
 
-app.config["MYSQL_HOST"]         = "localhost"
-app.config["MYSQL_USER"]         = "root"
-app.config["MYSQL_PASSWORD"]     = ""
-app.config["MYSQL_DB"]           = "hazardhub"
+app.config["MYSQL_HOST"]         = os.getenv("MYSQL_HOST", "localhost")
+app.config["MYSQL_USER"]         = os.getenv("MYSQL_USER", "root")
+app.config["MYSQL_PASSWORD"]     = os.getenv("MYSQL_PASSWORD", "")
+app.config["MYSQL_DB"]           = os.getenv("MYSQL_DB", "hazardhub")
 app.config["MYSQL_CURSORCLASS"]  = "DictCursor"
 app.config["UPLOAD_FOLDER"]      = UPLOAD_FOLDER
 app.config["MAX_CONTENT_LENGTH"] = 100 * 1024 * 1024   # 100 MB
